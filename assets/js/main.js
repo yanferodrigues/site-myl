e528b85be2428996e21de2d680b89ca38bb99dc4
Yan felipe <yanfelipe678@gmail.com>
2026-01-14 11:57:32 -0300

    alteração escritorio 11:55 14/01/2026

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let activeReportId = 'finance-main';
+function tabSelector(id,tab) {
+    activeReportId = id;
+    document.querySelectorAll(".finance-page").forEach(page => {page.classList.remove('active')});
+    document.getElementById(id).classList.add('active')
+
+    document.querySelectorAll(".finance-tab").forEach(page => {page.classList.remove('active')});
+    tab.classList.add('active')
+}
